4dcb37cfb06e3a28962d0ecbf6b0937d726c17a5
jayapalch25 <30292541+jayapalch25@users.noreply.github.com>
2017-07-22 17:43:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ element.innerHTML = "New Value";
 
 //Move the image to the right
 var img = document.getElementById('madi');
+var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
