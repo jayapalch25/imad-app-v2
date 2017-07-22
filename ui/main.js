568c29eb40d7fc8170e44f440a2fc8ba612ef364
jayapalch25 <30292541+jayapalch25@users.noreply.github.com>
2017-07-22 17:37:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML = "New Value";
 //Move the image to the right
 var img = document.getElementById('madi');
 img.onclick = function () {
-    img.style.marginleft = '100px';
+    img.style.marginLeft = '100px';
 };
